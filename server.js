a05a31e0e4ddcd21bf3b7641fb7ffe2a78f85caa
calimendoza <mbaivictor1@gmail.com>
2022-07-18 22:49:39 -0700

    added ejs files

--- server.js
+++ server.js
@@ -14,6 +14,8 @@ app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
 
 
-mongoose.connect(process.env.DB_CONNECTION)
+mongoose.connect(process.env.DB_CONNECTION, 
+    {useNewUrlParser: true},
+    () => {console.log('Connected to db!')})
 
 app.listen(PORT, () => console.log(`Server is running on ${PORT}`))
