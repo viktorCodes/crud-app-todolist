fc197a1645e26b2fd0e50552dece7dbb2fcf4249
calimendoza <mbaivictor1@gmail.com>
2022-07-18 21:50:02 -0700

    declared variables

---
+++ server.js
@@ -0,0 +1,10 @@
+//Declare Variables
+
+const express = require('express');
+const app = express();
+const PORT = 8500;
+const mongoose = require('mongoose');
+require('dotenv').config()
+//add model variable
+
+//
