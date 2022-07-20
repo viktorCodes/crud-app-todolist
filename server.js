b3dbd8fc762add98e42ccf08c220bac5bca4d7ac
calimendoza <mbaivictor1@gmail.com>
2022-07-19 22:32:38 -0700

    added delete method

--- server.js
+++ server.js
@@ -80,4 +80,16 @@ mongoose.connect(process.env.DB_CONNECTION,
             });
     });
 
+    //DELETE METHOD
+
+    app
+    .route("/remove/:id")
+    .get((req, res) => {
+        const id = req.params.id;
+        TodoTask.findByIdAndRemove(id, err => {
+            if (err) return res.send(500, err);
+            res.redirect("/");
+        });
+    });
+
 app.listen(PORT, () => console.log(`Server is running on ${PORT}`))
